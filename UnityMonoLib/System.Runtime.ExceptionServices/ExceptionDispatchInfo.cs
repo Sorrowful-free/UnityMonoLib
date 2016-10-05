eedb7a2ff17cb921fb67263010b50828f8bfb5bc
sorrowful_free <sorrowful.free@gmail.com>
2016-10-05 23:44:09 +0300

    exceptionDispatcherFix

--- UnityMonoLib/System.Runtime.ExceptionServices/ExceptionDispatchInfo.cs
+++ UnityMonoLib/System.Runtime.ExceptionServices/ExceptionDispatchInfo.cs
@@ -1,25 +1,15 @@
-using System.Reflection;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace System.Runtime.ExceptionServices
 {
 	public sealed class ExceptionDispatchInfo
 	{
+        public static readonly List<Exception> ExceptionsList = new List<Exception>();
 		readonly Exception _exception;
-		readonly object _source;
-		readonly string _stackTrace;
-		
-		const BindingFlags PrivateInstance = BindingFlags.Instance | BindingFlags.NonPublic;
-		static readonly FieldInfo RemoteStackTrace = typeof (Exception).GetField ("_remoteStackTraceString", PrivateInstance);
-		static readonly FieldInfo Source = typeof (Exception).GetField ("_source", PrivateInstance);
-		static readonly MethodInfo InternalPreserveStackTrace = typeof (Exception).GetMethod ("InternalPreserveStackTrace", PrivateInstance);
 		
 		private ExceptionDispatchInfo (Exception source)
 		{
 			_exception = source;
-			_stackTrace = _exception.StackTrace + Environment.NewLine;
-			_source = Source.GetValue (_exception);
 		}
 
 		public Exception SourceException { get { return _exception; } }
@@ -34,17 +24,7 @@ namespace System.Runtime.ExceptionServices
 
 		public void Throw ()
 		{
-			try
-			{
-				throw _exception;
-			}
-			catch
-			{
-				InternalPreserveStackTrace.Invoke (_exception, new object[0]);
-				RemoteStackTrace.SetValue (_exception, _stackTrace);
-				Source.SetValue (_exception, _source);
-				throw;
-			}
-		}
+            throw new Exception(_exception.Message, _exception); ;			
+        }
 	}
 }
